e92f457fab5e54b8f1fd71baf156ece253616309
Andrey Gershengoren <andrey.gershengoren@l-h.de>
2015-01-30 18:01:27 +0100

    Initial Commit

---
+++ Colors/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colors
+//
+//  Created by Andrey Gershengoren on 30.01.15.
+//  Copyright (c) 2015 Private. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Colors/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Colors
+//
+
+//  Copyright (c) 2015 Private. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
